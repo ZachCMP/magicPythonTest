92eec01a20016e3d09fa550e3ffa221b5ffc4fd9
ZachCMP <zparks@Allisons-iMac.local>
2019-07-31 16:15:55 -0400

    added more stuff

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import json
+import re
 
 with open('AllCards.json') as json_file:
     data = json.load(json_file)
-    theKey = data.keys()[0]
-    theRecord = data[theKey]
 
-    print theKey
-    print theRecord['colors']
+    dragonKeys = []
+
+    for key in data.keys():
+        if (re.match(r'[Dd]ragon', key)):
+            dragonKeys.append(key)
+
+    for key in dragonKeys:
+        print key + ' ' + str(data[key]['colors'])
+
+    # theKey = data.keys()[0]
+    # theRecord = data[theKey]
+
+    # print theKey
+    # print theRecord['colors']
